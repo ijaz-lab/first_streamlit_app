99979cb54fe726325bd0333f943f42f4abb3e8fb
IJAZ ALI <78338522+ijaz-lab@users.noreply.github.com>
2022-09-12 17:19:30 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit 
 
 streamlit.title('My parents New Healthy Diner'
-                'Breakfast Menu')
+                'Breakfast Menu'
+                'Omega 3 & Blueberry Oatmeal'
+                'Kale , Spinach &Rocket Smoothie'
+                'Hard-Boiled Free-Range Egg'
+          )
