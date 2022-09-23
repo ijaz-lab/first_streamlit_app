ebcad7dc93958d6a738e9e780febcb6cbcb55dfe
IJAZ ALI <78338522+ijaz-lab@users.noreply.github.com>
2022-09-24 01:03:19 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,37 +28,37 @@ streamlit.dataframe(fruits_to_show)
 
 #NEW Section to display fruityvice api response
 
-#streamlit.header("Fruityvice Fruit Advice!")
-#try:
-    #fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    #if not fruit_choice:
-         #streamlit.error("Please select a fruit to get information. ")
-    #else:
-        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        #streamlit.dataframe(fruityvice_normalized)
-#except URLError as e:
-    #streamlit.error()                                   
-                                        
-  
-# create the repeatable code block (Called a funtion)
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-
-#NEW section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
          streamlit.error("Please select a fruit to get information. ")
     else:
-	back_from_function = get_fruityvice_data(fruit_choice)
-	streamlit.dataframe(back_from_function)
-	
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-   streamlit.error()   
+    streamlit.error()                                   
+                                        
+  
+# create the repeatable code block (Called a funtion)
+#def get_fruityvice_data(this_fruit_choice):
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   # return fruityvice_normalized
+
+#NEW section to display fruityvice api response
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+    #fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    #if not fruit_choice:
+        # streamlit.error("Please select a fruit to get information. ")
+    #else:
+	#back_from_function = get_fruityvice_data(fruit_choice)
+	#streamlit.dataframe(back_from_function)
+	
+##except URLError as e:
+  # streamlit.error()   
 
 
                                              
