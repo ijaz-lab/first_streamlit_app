7a20e44162d5295d362bf081a9cbdcf032d272c4
IJAZ ALI <78338522+ijaz-lab@users.noreply.github.com>
2022-09-23 16:36:19 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,19 +28,28 @@ streamlit.dataframe(fruits_to_show)
 #NEW Section to display fruityvice api response
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#request
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?'')
+    #streamlit.write('The user entered ', fruit_choice)
+    if not fuit_choice:
+         streamlit.error("Please select a fruit to get information")
+    else:
+         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+         streamlit.dataframe(fruityvice_normalized)
+ except URLError as e:
+        streamlit.error()                                   
+                                        
+                                         
+                                              
+ #request
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # remove this <streamlit.text(fruityvice_response.json()) #just writes the data to the screen>
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 # donot run anything pas here while we troubleshoot
 streamlit.stop()
